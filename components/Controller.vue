131df1f9b4ea0fde890129c386ab4cfead4ff635
akatsuki1910 <akatsukicirno@gmail.com>
2021-10-11 23:44:44 +0900

    change bpm

---
+++ components/Controller.vue
@@ -0,0 +1,130 @@
+<template lang="pug">
+  div.controller
+    button.btn(@click='mcompile')
+      fa(:icon='faMusic')
+    button.btn(:disabled="!mFlag" @click='start')
+      fa(:icon='faPlay')
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator'
+import {
+  faHammer,
+  faRedo,
+  faMusic,
+  faPlay,
+} from '@fortawesome/free-solid-svg-icons'
+import Ace from '../components/Ace.vue'
+import Music from '../components/music'
+import soundShader from '../components/glsl/sound/sound.frag'
+import soundmainShader from '../components/glsl/sound/soundmain.frag'
+import sounddefinitionShader from '../components/glsl/sound/sounddefinition.frag'
+@Component({})
+export default class Controller extends Vue {
+  @Prop({ type: Ace }) readonly editor!: Ace
+  @Prop({ type: Number }) readonly bpm!: number
+
+  private gl: WebGLRenderingContext | null = null
+  private music = new Music()
+  private mFlag = false
+
+  // computed()
+  get getBPM() {
+    console.log(1)
+    return this.bpm
+  }
+
+  get faHammer() {
+    return faHammer
+  }
+
+  get faRedo() {
+    return faRedo
+  }
+
+  get faMusic() {
+    return faMusic
+  }
+
+  get faPlay() {
+    return faPlay
+  }
+
+  @Watch('getBPM')
+  setBPM(bpm: number) {
+    console.log(2)
+    this.changeBPM(bpm)
+  }
+
+  @Watch('editor')
+  setValue(ace: Ace) {
+    ace.setValue(soundShader)
+  }
+
+  // methods()
+  mcompile() {
+    const ss =
+      sounddefinitionShader + this.editor!.editor.getValue() + soundmainShader
+    this.mFlag = this.compileCheck(ss)
+    if (this.mFlag) {
+      this.music.setFrag(ss)
+    }
+  }
+
+  start() {
+    this.mFlag = false
+    this.changeBPM(this.bpm)
+    this.music.pp()
+  }
+
+  changeBPM(bpm: number) {
+    if (this.music.node) {
+      if (bpm > 0) {
+        this.music.node.loopEnd = 60 / bpm
+      }
+    }
+  }
+
+  compileCheck(s: string) {
+    let rFrag = false
+    const canvas: HTMLCanvasElement = document.createElement('canvas')
+    if (!this.gl) {
+      this.gl = canvas.getContext('webgl')!
+    }
+    const gl = this.gl!
+    const k = gl.createShader(gl.FRAGMENT_SHADER)!
+    gl.shaderSource(k, s)
+    gl.compileShader(k)
+    if (!gl.getShaderParameter(k, gl.COMPILE_STATUS)) {
+      window.alert(gl.getShaderInfoLog(k))
+    } else {
+      rFrag = true
+    }
+    return rFrag
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+
+.edi {
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  height: 100vh;
+}
+
+.controller {
+  width: 20vw;
+}
+
+.btn {
+  background-color: transparent;
+}
+.bpm {
+  width: 4rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,57 @@
 <template lang="pug">
   div.container
-    Editor
-    Editor
+    .edi
+      Ace(ref="sound")
+    .controller
+      div
+        | BPM
+        input.bpm(type="number" min="1" step="1" v-model="bpm")
+      Controller(:editor='s1' :bpm='getBPM')
+      Controller(:editor='s2' :bpm='getBPM')
+    .edi
+      Ace(ref="sound2")
 </template>
+
+<script lang="ts">
+import { Component, Ref, Vue } from 'nuxt-property-decorator'
+import Ace from '../components/Ace.vue'
+@Component({
+  components: {
+    Ace,
+  },
+})
+export default class Index extends Vue {
+  @Ref() sound!: Ace
+  @Ref() sound2!: Ace
+
+  s1: Ace | null = null
+  s2: Ace | null = null
+  bpm = 60
+
+  get getBPM() {
+    return +this.bpm
+  }
+
+  mounted() {
+    this.s1 = this.sound
+    this.s2 = this.sound2
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .container {
   display: flex;
+}
+
+.edi {
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  height: 100vh;
+}
+
+.controller {
+  width: 20vw;
 }
 </style>
